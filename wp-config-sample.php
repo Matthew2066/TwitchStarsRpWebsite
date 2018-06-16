b28a9660a09162095241ea9ce0e15a42c1c49c29
Matthew <40185554+Matthew2066@users.noreply.github.com>
2018-06-16 20:54:40 +0100

    Keys added

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'twitchst_website');
 
 /** MySQL database username */
-define('DB_USER', 'website');
+define('DB_USER', 'twitchst_website');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'TROLLtroll12345!');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '<<& +3Vs(D?q&$e C}b+LSA+*-?rOE|+yhV>xWYO[X9,&%U5IDIleEe^1%1_ dRd');
+define('SECURE_AUTH_KEY',  '?GY-8G)aX:ZVTtX|oNnjWy7+VBgT]:~`Nnl.0whWI}2&|lws&p$&bfQP+I|}smG5');
+define('LOGGED_IN_KEY',    'S+md;h[?=IoaYcI3B<:h>xD={8akM+5fYj|JXq5I--982&#Q7ge|[ x3D:)9OW$c');
+define('NONCE_KEY',        '~|O_f?Ihc8+Q-)OO=HFUYRN48[cnJ}rMdAB#?~.4$pHgrC*(F?~p{jFEzysR^KR3');
+define('AUTH_SALT',        '5}IMy[;IFX2$g#G6*I1/:5CQo9h:Sb&KSZ}P+;Db0/:e-2-6ftt~k2ggN6U}kelN');
+define('SECURE_AUTH_SALT', 'VC>~vnVeORd72[S0MZ-Q0T%|]T{3Eal$v9w,/H ~p7s.m1rO2)?+#*itecI1m-F?');
+define('LOGGED_IN_SALT',   'h;)PtCS3Tz]gq%k(9FBlJeZwyF_frM4^-EjJBlY4Q|M]@?{B[P(0H`yZWjvEd8E ');
+define('NONCE_SALT',       ')fECNbJ.?Cukz%NHgSMn5l=feCZH0+9vM?_L3/5V#g;OL]q5$%?WzCUP}NNznR8O');
 
 /**#@-*/
 
